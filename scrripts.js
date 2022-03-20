9e678a532c32ab53a9fd7b7e7c53171ccdfe72f7
Anurag <2006535@kiit.ac.in>
2022-03-20 11:49:58 +0530

    Some changes in designing

---
+++ scrripts.js
@@ -0,0 +1,14 @@
+$("#mycarousel").carousel({ interval: 2000 });
+            $("#carouselButton").click(function(){
+                if($('#carouselButton').children("span").hasClass('fa-pause')){  
+                $("#mycarousel").carousel('pause');
+                $('#carouselButton').children("span").removeClass('fa-pause');
+                $('#carouselButton').children("span").addClass('fa-play');
+                }
+                else if ($('#carouselButton').children("span").hasClass('fa-play'))
+                {   
+                   $("#mycarousel").carousel('cycle');
+                   $('#carouselButton').children("span").removeClass('fa-play');
+                   $('#carouselButton').children("span").addClass('fa-pause');
+                   }
+            });
